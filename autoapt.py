75d5a2e3bc9a7e7b9d837e826319b3b7ff5e2558
Michał J. Gajda <mjgajda@migamake.com>
2022-02-22 10:55:07 +0100

    Working Python3 version.

--- autoapt.py
+++ autoapt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import __builtin__, subprocess, sys
+import subprocess, sys
 """This module allows to run auto-apt to find missing python modules.
    It is most convenient when chained as a part of sys.excepthook.
    Try putting the following into your .pythonrc:
@@ -23,57 +23,68 @@ ImportError: No module named pstats you may try packages: python-profiler python
 __all__ = ["auto_apt", "excepthook", "install_hook"]
 
 def auto_apt(name):
-  #print "name", repr(name)
+  """
+  For a given import name, find a file that may provide it with `apt-file`.
+  """
   proc = subprocess.Popen(["apt-file", "search", name.replace(".", "/")],
-                          stdout = subprocess.PIPE,
-                          stderr = subprocess.PIPE)
+                          stdout   = subprocess.PIPE,
+                          stderr   = subprocess.PIPE,
+                          encoding = "ascii")
   out, err = proc.communicate()
   if err.strip() != "":
-    print >>sys.stderr, "auto-apt error:", err
+    print("auto-apt error: %s" % err, file=sys.stderr)
   results = set([])
   for line in out.split("\n"):
-    #print repr(line)
     if (".py" not in line) and ("python3" not in line): # false positive?
       continue
-    if "\t" not in line: # error in output format?
+    if ":" not in line: # error in output format?
+      print("Error in apt-file output: expected \\t", file=sys.stderr)
       continue # may be changed to raise exception WHEN debugging
-    results.add(line[line.index("\t"):].split("/")[-1])
+    results.add(line.split(":")[0])
   return results
 
-original_excepthook = sys.excepthook
 def excepthook(exctype, exc, trace):
+  """
+  Unhandled exception hook:
+  If exception is ImportError,
+  then call `auto_apt` function to discover the Apt package to install.
+  """
   if exctype == ImportError:
     missing_module_string = "No module named "
     if exc.args[0].startswith(missing_module_string):
       name = exc.args[0][len(missing_module_string):]
       packages = " ".join(auto_apt(name))
-    exc.args = ("%s you may try packages: %s" % (exc.args[0], packages),)
+    exc.args = ("For %s you may try one of the packages: %s"
+                   % (exc.args[0], packages),)
   original_excepthook(exctype, exc, trace)
 
 def install_hook():
+  "Install a global hook of unhandled exceptions."
   global original_excepthook
   original_excepthook = sys.excepthook
   sys.excepthook = excepthook
 
+install_hook()
+
+# Debugging as a module
 if __name__=="__main__":
   import optparse
   # Options
   optparser = optparse.OptionParser(usage="%prog [<options>] <module> - finds packages responsible for named modules")
-  
+
   opts, args = optparser.parse_args() # Parse arguments
-  
+
   # Verify arguments
   if len(args)<1:
-    print >> sys.stderr, optparser.format_help()
+    print(optparser.format_help(), file=sys.stderr)
     sys.exit(1)
- 
-  install_hook() 
-  for name in args:
-    print name, ":", ' '.join(auto_apt(name))
 
-    cmd = "import %s" % name
-    #print "cmd", repr(cmd)
-    exec cmd
+  for name in args:
+    result = auto_apt(name)
+    print("%s: %s" % (name, ' '.join(auto_apt(name))))
+
+    cmd = "import " + name
+    exec(cmd)
 
 
      
