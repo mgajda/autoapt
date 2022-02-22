f57084a113d67828160c0ddbe1a1a4cc9a7f5b67
Michał J. Gajda <mjgajda@migamake.com>
2022-02-22 13:35:32 +0100

    Improving test.

--- pack.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-zip autoapt.zip autoapt.py LICENSE README
--- test_install.sh
+++ test_install.sh
@@ -9,8 +9,10 @@ python3 -m twine upload --repository testpypi dist/* \
                         --verbose &&
 sync &&
 echo "VERSION='${VERSION}'" &&
+sensible-browser "https://test.pypi.org/project/autoapt/${VERSION}/" &&
 sleep 20 && # time for index update
 eval python3 -m pip install  --index-url "https://test.pypi.org/simple/" \
                              --force-reinstall \
+                             --no-cache-dir    \
                              --no-deps "autoapt==${VERSION}" &&
 python3 test/test.py
